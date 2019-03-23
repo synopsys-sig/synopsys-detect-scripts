f1dfea91a484d174160e1d114d20f00421097445
Builder Service <serv-builder@int-ubuntu-jenkins02.dc1.lan>
2019-03-23 16:07:18 -0400

    Committing the latest update-site contents to gh-pages branch.

--- detect-2.0.0-SNAPSHOT.sh
+++ detect-2.0.0-SNAPSHOT.sh
@@ -10,7 +10,7 @@ DETECT_RELEASE_VERSION=${DETECT_LATEST_RELEASE_VERSION}
 # *that* key will be used to get the download url from
 # artifactory. These DETECT_VERSION_KEY values are
 # properties in Artifactory that resolve to download
-# urls for the detect jar file. As of 2019-03-22, the
+# urls for the detect jar file. As of 2019-03-23, the
 # available DETECT_VERSION_KEY values are:
 # DETECT_LATEST, DETECT_LATEST_4, DETECT_LATEST_5
 # Every new major version of detect will have its own
@@ -111,7 +111,7 @@ get_detect() {
 
 run_detect() {
   JAVACMD="java ${DETECT_JAVA_OPTS} -jar ${DETECT_DESTINATION}"
-  echo "running detect: ${JAVACMD} ${LOGGABLE_SCRIPT_ARGS}"
+  echo "running Detect: ${JAVACMD} ${LOGGABLE_SCRIPT_ARGS}"
 
   # first, silently delete (-f ignores missing
   # files) any existing shell script, then create
