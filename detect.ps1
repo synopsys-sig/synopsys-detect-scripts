7bc90a9ef9857052c91939b07b4057b586872b44
Builder Service <serv-builder@int-ubuntu-jenkins02.dc1.lan>
2020-05-27 15:51:25 -0400

    Committing the latest update-site contents to gh-pages branch.

--- detect.ps1
+++ detect.ps1
@@ -13,7 +13,7 @@ $EnvDetectDesiredVersion = Get-EnvironmentVariable -Key "DETECT_LATEST_RELEASE_V
 # *that* key will be used to get the download url from
 # artifactory. These DETECT_VERSION_KEY values are
 # properties in Artifactory that resolve to download
-# urls for the detect jar file. As of 2020-01-16, the
+# urls for the detect jar file. As of 2020-05-27, the
 # available DETECT_VERSION_KEY values are:
 #
 # Every new major version of detect will have its own
@@ -87,7 +87,7 @@ $DownloadOnly = Get-EnvironmentVariable -Key "DETECT_DOWNLOAD_ONLY" -DefaultValu
 # heap size, you would set DETECT_JAVA_OPTS=-Xmx6G.
 # $DetectJavaOpts = Get-EnvironmentVariable -Key "DETECT_JAVA_OPTS" -DefaultValue "";
 
-$Version = "2.3.0"
+$Version = "2.4.0"
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 #Enable TLS2
 
@@ -243,11 +243,9 @@ function Get-DetectJar ($DetectFolder, $DetectSource, $DetectVersionKey, $Detect
     if ($DetectSource) {
         Write-Host "Using Detect source $DetectSource"
 
-        $DetectVersion = Parse-Version -DetectSource $DetectSource
+        $DetectFileName = Parse-Detect-File-Name -DetectSource $DetectSource
 
-        Write-Host "Using Detect Version $DetectVersion"
-
-        $DetectJarFile = "$DetectFolder/synopsys-detect-$DetectVersion.jar"
+        $DetectJarFile = "$DetectFolder/$DetectFileName"
     } else {
         Write-Host "Unable to find Detect Source, will attempt to find a last downloaded detect."
 
@@ -277,13 +275,10 @@ function Get-DetectJar ($DetectFolder, $DetectSource, $DetectVersionKey, $Detect
     return $DetectJarFile
 }
 
-function Parse-Version($DetectSource) {
+function Parse-Detect-File-Name($DetectSource) {
     $SlashParts = $DetectSource.Split("/")
     $LastPart = $SlashParts[$SlashParts.Length - 1]
-    $DashParts = $LastPart.Split("-")
-    $VersionJar = $DashParts[$DashParts.Length - 1]
-    $Version = $VersionJar.TrimEnd(".jar")
-    return $Version
+    return $LastPart
 }
 
 function Invoke-Detect ($DetectJarFile, $DetectArgs) {
