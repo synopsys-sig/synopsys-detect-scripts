1e09893c953bced19a931564742a3d23e8d1b0e2
Builder Service <serv-builder@int-ubuntu-jenkins02.dc1.lan>
2020-06-18 17:26:14 -0400

    Committing the latest update-site contents to gh-pages branch.

--- detect-2.4.1-SNAPSHOT.sh
+++ detect-2.4.1-SNAPSHOT.sh
@@ -21,7 +21,7 @@ DETECT_RELEASE_VERSION=${DETECT_LATEST_RELEASE_VERSION}
 # *that* key will be used to get the download url from
 # artifactory. These DETECT_VERSION_KEY values are
 # properties in Artifactory that resolve to download
-# urls for the detect jar file. As of 2020-06-17, the
+# urls for the detect jar file. As of 2020-06-18, the
 # available DETECT_VERSION_KEY values are:
 #
 # Every new major version of detect will have its own
