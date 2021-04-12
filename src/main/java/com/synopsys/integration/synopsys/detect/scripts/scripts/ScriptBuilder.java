cfddfd72964230eb5a34860f9c61cad42bff07e0
Steve Billings <sbillings@blackducksoftware.com>
2021-04-12 14:06:42 -0400

    style: removed some old redundant copyright headers

--- src/main/java/com/synopsys/integration/synopsys/detect/scripts/scripts/ScriptBuilder.java
+++ src/main/java/com/synopsys/integration/synopsys/detect/scripts/scripts/ScriptBuilder.java
@@ -5,28 +5,7 @@
  *
  * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
  */
-package com.synopsys.integration.synopsys.detect.scripts.scripts; /**
- * synopsys-detect-scripts
- *
- * Copyright (c) 2019 Synopsys, Inc.
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+package com.synopsys.integration.synopsys.detect.scripts.scripts;
 
 import java.io.File;
 import java.io.IOException;
@@ -54,28 +33,6 @@ import com.synopsys.integration.rest.request.Request;
 import com.synopsys.integration.rest.request.Response;
 import com.synopsys.integration.util.ResourceUtil;
 
-/**
- * synopsys-detect-scripts
- *
- * Copyright (c) 2019 Synopsys, Inc.
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 public class ScriptBuilder {
     // To add a new Detect Major version (to generate scripts for), just increment getDetectMajorVersionLatest:
     private final int getDetectMajorVersionLatest = 7;
